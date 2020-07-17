dc615500c1c44ae3017bea38f2dddc559109419b
Yashika Aggarwal <yashuanshu0@gmail.com>
2020-07-18 03:41:07 +0530

    Servlet Step-4 Post Creation

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,15 +25,15 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+    
+    ArrayList<String> list=new ArrayList<String>();
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // response.setContentType("text/html;");
     // response.getWriter().println("Hello Yashika Aggarwal!<br>");
     // response.getWriter().println("Cool!!");
-    ArrayList<String> list=new ArrayList<String>();
-    list.add("Hi! Yashika");
-    list.add("Great!");
+    // list.add("Hi! Yashika");
+    // list.add("Great!");
 
     //Converting arraylist to jsonarray
 
@@ -44,5 +44,20 @@ public class DataServlet extends HttpServlet {
     response.getWriter().print(json) ;
 
   }
+
+   @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    String value = request.getParameter("cmnt");
+    String text = "" ;
+    if (value != null) {
+      text = value;
+    }
+    list.add(text);
+    response.setContentType("text/html;");
+    response.getWriter().println(text);
+    response.sendRedirect("/index.html");
+  }
+
 
 }
